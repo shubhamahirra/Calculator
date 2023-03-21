fa577ec77dc312c1b9b68f86ad2e7a45afb2d015
Shubham Ahirrao <shubhamahirrao4444@gmail.com>
2023-03-21 18:17:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Calc from './components/Calc';
+
+function App() {
+  return (
+    <div className="App">
+      <Calc/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
